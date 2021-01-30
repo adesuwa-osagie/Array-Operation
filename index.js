9d1ca791842e71f2d5604bccbac694890f9d9982
osagie_dot_developer <adesuwa.osagie@aol.com>
2021-01-30 21:35:03 +0000

    Solution w assertion function.

--- index.js
+++ index.js
@@ -20,3 +20,49 @@ arrayOperation(15, 20, 7) ➞ []
 
 */
 
+function arrayOperation(x, y, n) {
+  //create a result array
+  let result = [];
+  
+  //iterate from index = x to y(included)
+  for (let i = x; i <= y; i++) {
+    //if current number is divisible by n
+    if(i % n === 0) {
+      //push number into result array
+      result.push(i);
+    }
+  }
+  //return result
+  return result;
+}
+
+let inputX = 1;
+let inputY = 10;
+let integer = 3;
+let actual = arrayOperation(inputX, inputY, integer);
+let expected = [3, 6, 9];
+
+console.log(actual)
+
+function assertArraysEquals(actual, expected, testName) {
+  //same length
+  let sameLength = actual.length === expected.length;
+
+  //same values
+  let sameValues = true;
+
+  for (let i = 0; i < expected.length; i++) {
+    if (actual[i] !== expected[i]) {
+      sameValues = false;
+      break;
+    }
+  }
+
+  if (sameLength && sameValues) {
+    console.log('test passed');
+  } else {
+    console.log(`FAILED [${testName}] Expected ${expected}, but got ${actual}`);
+  }
+}
+
+console.log( assertArraysEquals(actual, expected, 'array operation') )
